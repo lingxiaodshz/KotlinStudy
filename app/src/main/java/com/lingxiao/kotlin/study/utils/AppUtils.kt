e1bc4d0795174ef7003a974eeece5f9eb3b2b627
lingxiaodshz <lingxiaodshz@163.com>
2020-09-23 23:46:35 +0800

    工具类

--- app/src/main/java/com/lingxiao/kotlin/study/utils/AppUtils.kt
+++ app/src/main/java/com/lingxiao/kotlin/study/utils/AppUtils.kt
@@ -2,25 +2,29 @@ package com.lingxiao.kotlin.study.utils
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 
 /**
  * @author luckw
  * @date   2020/9/4
  */
-class AppUtils private constructor() {
+object AppUtils {
 
-    companion object {
-
-        fun getVersionName(context: Context): String {
-            var versionName = ""
-            try {
-                val packageName = context.packageName
-                versionName = context.packageManager
-                        .getPackageInfo(packageName, 0).versionName
-            } catch (e: PackageManager.NameNotFoundException) {
-                e.printStackTrace()
-            }
-            return versionName
+    fun getVersionName(context: Context): String {
+        var versionName = ""
+        try {
+            val packageName = context.packageName
+            versionName = context.packageManager
+                    .getPackageInfo(packageName, 0).versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
+        return versionName
+    }
+
+    fun getMobileModel(): String {
+        var model: String = Build.MODEL
+        model = model?.trim { it <= ' ' } ?: ""
+        return model
     }
 }
